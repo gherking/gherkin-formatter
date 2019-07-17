eca940293614b9360c28b293a865d1b1b0f70cc6
Sándor Orosz <os.sandor@gmail.com>
2019-07-17 17:23:36 +0200

    lint fixes

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,10 +1,11 @@
-import { format, FormatOptions } from "../src";
 import { Document } from "gherkin-ast";
+import { format, FormatOptions } from "../src";
 
 describe("gherkin-formatter", () => {
     test("should pass", () => {
         const document: Document = new Document("uri");
-        const options: FormatOptions = {lineBreak:"\n"} as FormatOptions; 
-        expect(format(document, options)).toBe("{\"document\":{\"uri\":\"uri\",\"feature\":null},\"options\":{\"lineBreak\":\"\\n\"}}");
+        const options: FormatOptions = { lineBreak: "\n" } as FormatOptions;
+        expect(format(document, options))
+            .toBe("{\"document\":{\"uri\":\"uri\",\"feature\":null},\"options\":{\"lineBreak\":\"\\n\"}}");
     });
 });
